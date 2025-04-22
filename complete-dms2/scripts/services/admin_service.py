76717baf74001bf5958b3c7b628a0c4dda0a3649
alfiya <alfiyaajish@gmail.com>
2025-04-22 13:14:10 +0530

    committed

--- complete-dms2/scripts/services/admin_service.py
+++ complete-dms2/scripts/services/admin_service.py
@@ -1,5 +1,56 @@
-from fastapi import APIRouter, Depends, status, HTTPException
-from fastapi.requests import Request
+# from fastapi import APIRouter, Depends, status, HTTPException
+# from fastapi.requests import Request
+# from scripts.handlers.admin_handler import (
+#     list_all_users,
+#     get_user_details,
+#     delete_user,
+#     list_all_containers
+# )
+# from scripts.constants.api_endpoints import Endpoints
+# from scripts.logging.logger import logger
+# from scripts.utils.jwt_utils import get_current_user_from_token
+#
+# admin_router = APIRouter()
+#
+#
+# def admin_required(request: Request):
+#     user = get_current_user_from_token(request)
+#     if user.role != "Admin":
+#         logger.warning(f"User '{user.username}' attempted to access restricted route without admin privileges")
+#         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+#                             detail="You don't have permission to perform this action.")
+#     return user
+#
+#
+# @admin_router.get(Endpoints.ADMIN_USERS_LIST, status_code=status.HTTP_200_OK)
+# def list_users(user: dict = Depends(admin_required)):
+#
+#     logger.info("Request to fetch all users")
+#     return list_all_users(user)
+#
+#
+# @admin_router.get(Endpoints.ADMIN_USER_DETAILS, status_code=status.HTTP_200_OK)
+# def get_user_info(username: str, user: dict = Depends(admin_required)):
+#
+#     logger.info(f"Request to fetch details of user '{username}'")
+#     return get_user_details(username, user)
+#
+#
+# @admin_router.delete(Endpoints.ADMIN_USER_DELETE, status_code=status.HTTP_200_OK)
+# def delete_user_account(username: str, user: dict = Depends(admin_required)):
+#
+#     logger.info(f"Request to delete user '{username}'")
+#     return delete_user(username, user)
+#
+#
+# @admin_router.get(Endpoints.ADMIN_CONTAINERS_LIST, status_code=status.HTTP_200_OK)
+# def list_containers(user: dict = Depends(admin_required)):
+#
+#     logger.info("Request to fetch all containers")
+#     return list_all_containers(user)
+
+
+from fastapi import APIRouter, Depends, status
 from scripts.handlers.admin_handler import (
     list_all_users,
     get_user_details,
@@ -8,43 +59,27 @@ from scripts.handlers.admin_handler import (
 )
 from scripts.constants.api_endpoints import Endpoints
 from scripts.logging.logger import logger
+from scripts.models.jwt_model import TokenData
 from scripts.utils.jwt_utils import get_current_user_from_token
 
 admin_router = APIRouter()
 
-
-def admin_required(request: Request):
-    user = get_current_user_from_token(request)
-    if user.role != "Admin":
-        logger.warning(f"User '{user.username}' attempted to access restricted route without admin privileges")
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                            detail="You don't have permission to perform this action.")
-    return user
-
-
 @admin_router.get(Endpoints.ADMIN_USERS_LIST, status_code=status.HTTP_200_OK)
-def list_users(user: dict = Depends(admin_required)):
-
+def list_users(user: TokenData = Depends(get_current_user_from_token)):
     logger.info("Request to fetch all users")
     return list_all_users(user)
 
-
 @admin_router.get(Endpoints.ADMIN_USER_DETAILS, status_code=status.HTTP_200_OK)
-def get_user_info(username: str, user: dict = Depends(admin_required)):
-
+def get_user_info(username: str, user: TokenData = Depends(get_current_user_from_token)):
     logger.info(f"Request to fetch details of user '{username}'")
     return get_user_details(username, user)
 
-
 @admin_router.delete(Endpoints.ADMIN_USER_DELETE, status_code=status.HTTP_200_OK)
-def delete_user_account(username: str, user: dict = Depends(admin_required)):
-
+def delete_user_account(username: str, user: TokenData = Depends(get_current_user_from_token)):
     logger.info(f"Request to delete user '{username}'")
     return delete_user(username, user)
 
-
 @admin_router.get(Endpoints.ADMIN_CONTAINERS_LIST, status_code=status.HTTP_200_OK)
-def list_containers(user: dict = Depends(admin_required)):
-
+def list_containers(user: TokenData = Depends(get_current_user_from_token)):
     logger.info("Request to fetch all containers")
     return list_all_containers(user)
